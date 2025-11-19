bcfa13597868add07eaca65c93398517bb523844
Emmanuel01-cyber <emmanuel.sunday@binghamuni.edu.ng>
2025-11-19 23:39:15 +0000

    Update Hello1.py to include name in greeting

--- Hello1.py
+++ Hello1.py
@@ -3,7 +3,7 @@ git clone https://github.com/Emmanuel01-cyber/Assessment.git
 cd Assessment
 
 # 2. Create the file
-echo 'print("Hello World")' > Hello1.py
+echo 'print("Hello World, my name is Emmanuel")' > Hello1.py
 
 # 3. Add, commit, push
 git add Hello1.py

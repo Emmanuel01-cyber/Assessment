09f13b5ea1d4d26d1cb87e696ab5c9f7fe0dd9b2
Emmanuel01-cyber <emmanuel.sunday@binghamuni.edu.ng>
2025-11-19 23:33:38 +0000

    Add instructions for creating Hello1.py

---
+++ Hello1.py
@@ -0,0 +1,11 @@
+# 1. Clone the repo
+git clone https://github.com/Emmanuel01-cyber/Assessment.git
+cd Assessment
+
+# 2. Create the file
+echo 'print("Hello World")' > Hello1.py
+
+# 3. Add, commit, push
+git add Hello1.py
+git commit -m "Add Hello1.py that prints Hello World"
+git push origin main
